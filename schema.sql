daf1ad8058c9e55aea5a4864b302bf4b0d69c779
goldenst <jeff@goldenstatetow.com>
2019-03-19 18:47:23 -0700

    added items to database

--- schema.sql
+++ schema.sql
@@ -26,3 +26,21 @@ VALUES ('Hose', '50 Foot Gargen  Hose', 25, 10, 'Gardning' );
 
 INSERT INTO products (name, descript, price, quan, dept )
 VALUES ('CD', 'Blank CD Pack of 50', 50, 50, 'Computers' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('Bike', 'Boys 10 Speed Bike', 250, 10, 'sports' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('skates', 'Roller Skates', 75, 21, 'sports' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('tv', '55In panasonic tv', 350, 10, 'electronics' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('iphone', 'iphone X 64g', 950, 10, 'electronics' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('frisbee', '12in glow in the dark frisbee', 10, 100, 'sports' );
+
+INSERT INTO products (name, descript, price, quan, dept )
+VALUES ('gloves', 'mens xl lether work gloves', 5, 100, 'sports' );
